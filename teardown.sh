49c018c9ba83e0de8572c5315d25e558bed0df1a
haresudhan <haresudhan@zoho.com>
2020-05-04 23:52:26 -0700

    Config files added

---
+++ teardown.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+RED='\033[0;31m'
+NC='\033[0m'
+
+docker ps
+
+echo -e "${RED}killing containers,${NC}"
+docker stop iroha
+docker stop some-postgres
+
+echo -e "${RED}removing rest${NC}"
+docker system prune -a -f 
+docker volume rm blockstore
+docker network prune -f 
+
+echo -e "${RED}check the processes${NC}"
+docker container ls
+echo -e "${RED}Images${NC}"
+docker image ls
+echo -e "${RED}Volumes${NC}"
+docker volume ls
+echo -e "${RED}Network${NC}"
+docker network ls
