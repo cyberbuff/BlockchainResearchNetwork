1252a759afa9eed1f59e76a5bbd8d23583a63612
haresudhan <haresudhan@zoho.com>
2020-05-05 08:41:38 -0700

    Genesis block

--- flask-project/auth.py
+++ flask-project/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import User
+from models import User,Journal
 from app import db
 from flask_login import login_user, logout_user, login_required
 
--- flask-project/models.py
+++ flask-project/models.py
@@ -9,3 +9,9 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
     private_key = db.Column(db.String(1000),unique=True)
+
+
+class Journal(UserMixin,db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    hash = db.Column(db.String(1000),unique=True)
+    title = db.Column(db.String(1000))
